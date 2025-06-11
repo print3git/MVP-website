708f33dbd03fa0c1020c7a55bfb2816f415deb07
print3git <JekyllC987123@outlook.com>
2025-06-11 20:14:01 +0100

    ci: fix eslint config for cjs

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -1,8 +1,8 @@
-import js from '@eslint/js';
-import prettier from 'eslint-config-prettier';
-import globals from 'globals';
+const js = require('@eslint/js');
+const prettier = require('eslint-config-prettier');
+const globals = require('globals');
 
-export default [
+module.exports = [
   {
     languageOptions: {
       globals: {
