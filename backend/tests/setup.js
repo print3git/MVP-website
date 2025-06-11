89fecdd0e26cecd2ab3aa01095d48e488ed4b11e
print3git <JekyllC987123@outlook.com>
2025-06-11 22:18:23 +0100

    Run formatter

--- backend/tests/setup.js
+++ backend/tests/setup.js
@@ -21,7 +21,6 @@ if (typeof global.setImmediate === 'undefined') {
   global.setImmediate = (cb) => setTimeout(cb, 0);
 }
 
-beforeEach(() => {
   jest.spyOn(console, 'error').mockImplementation((...args) => {
     throw new Error('console.error called: ' + args.join(' '));
   });
@@ -29,7 +28,6 @@ beforeEach(() => {
 
 // Clean up between tests
 afterEach(() => {
-  jest.restoreAllMocks();
 
   if (global.window && typeof global.window.close === 'function') {
     global.window.close();
