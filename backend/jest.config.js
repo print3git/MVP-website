b1ce8611de828dbb8405d70a95aa6a4a475f4222
print3git <JekyllC987123@outlook.com>
2025-06-11 16:11:21 +0100

    Update jest.config.js

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,4 +1,6 @@
+// backend/jest.config.js
 module.exports = {
+  rootDir: '.',
   setupFilesAfterEnv: ['<rootDir>/tests/setup.js'],
   testEnvironment: 'node',
   testMatch: ['<rootDir>/tests/**/*.test.js'],
