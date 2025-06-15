79e2148e74db0058a205538a1c38536ce9eb1873
print3git <JekyllC987123@outlook.com>
2025-06-15 23:11:36 +0100

    Remove rating from stats ticker

--- js/index.js
+++ js/index.js
@@ -600,12 +600,11 @@ async function init() {
       .then((r) => (r.ok ? r.json() : null))
       .then((data) => {
         const prints = data?.printsSold ?? 42;
-        const rating = data?.averageRating ?? 4.8;
-        el.textContent = `\u{1F525} ${prints} prints sold in the last 24 h \u00A0 \u2022 \u00A0 \u2B50 ${rating}/5 average rating`;
+        el.innerHTML = `\u{1F525} ${prints} prints sold<br>in the last 24 h`;
       })
       .catch(() => {
-        el.textContent =
-          '\u{1F525} 42 prints sold in the last 24 h \u00A0 \u2022 \u00A0 \u2B50 4.8/5 average rating';
+        el.innerHTML =
+          '\u{1F525} 42 prints sold<br>in the last 24 h';
       });
   }
 
