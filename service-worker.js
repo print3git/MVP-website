a355ba5ed0d432032ee953197713c629aa2d53b4
print3git <JekyllC987123@outlook.com>
2025-06-17 18:44:32 +0100

    Add service worker and defer scripts

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'model-cache-v1';
+const ASSETS = [
+  'https://modelviewer.dev/shared-assets/models/Astronaut.glb',
+  'https://modelviewer.dev/shared-assets/environments/neutral.hdr'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (ASSETS.some(url => event.request.url.startsWith(url))) {
+    event.respondWith(
+      caches.match(event.request).then(resp => {
+        return resp || fetch(event.request).then(response => {
+          const clone = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+          return response;
+        });
+      })
+    );
+  }
+});
