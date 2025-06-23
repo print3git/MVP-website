929310f2fd13b70bad3abd8761091850b9b13e18
print3git <JekyllC987123@outlook.com>
2025-06-23 16:34:06 +0100

    fix: ensure coverage collection and lockfile sync

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -13,7 +13,7 @@ module.exports = {
 module.exports = {
   ...module.exports,
   collectCoverage: true,
-  collectCoverageFrom: ["backend/**/*.{js,jsx,ts,tsx}"],
+  collectCoverageFrom: ["**/*.{js,jsx,ts,tsx}"],
   coveragePathIgnorePatterns: [
     "<rootDir>/backend/db.js",
     "<rootDir>/backend/shipping.js",
