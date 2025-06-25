ffede6b7bc326a79ca2eba47d0d656d8bf21c7ff
print3git <JekyllC987123@outlook.com>
2025-06-25 14:23:38 +0100

    feat: add tracking pixel and social proof

---
+++ js/trackingPixel.js
@@ -0,0 +1,9 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const img = document.createElement("img");
+  img.src = "/pixel";
+  img.alt = "";
+  img.width = 1;
+  img.height = 1;
+  img.style.display = "none";
+  document.body.appendChild(img);
+});
