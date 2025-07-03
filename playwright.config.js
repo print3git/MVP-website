843dcf3c808fb2508833e5aa4af61a93ec24f54b
print3git <JekyllC987123@outlook.com>
2025-07-03 15:05:30 +0100

    ci: reuse existing server in playwright

--- playwright.config.js
+++ playwright.config.js
@@ -1,14 +1,15 @@
-const { defineConfig } = require('@playwright/test');
-const baseURL = process.env.PLAYWRIGHT_BASE_URL || 'http://localhost:3000';
+const { defineConfig } = require("@playwright/test");
+const baseURL = process.env.PLAYWRIGHT_BASE_URL || "http://localhost:3000";
 
 module.exports = defineConfig({
-  testDir: 'e2e',
+  testDir: "e2e",
   use: { baseURL, headless: true },
   webServer: process.env.PLAYWRIGHT_BASE_URL
     ? undefined
     : {
-        command: 'npx http-server -c-1 -p 3000',
+        command: "npx http-server -c-1 -p 3000",
         port: 3000,
         timeout: 120 * 1000,
+        reuseExistingServer: true,
       },
 });
