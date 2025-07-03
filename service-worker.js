85d8bda1068dd5244657dedde0090015f53f3a80
print3git <JekyllC987123@outlook.com>
2025-07-03 16:01:23 +0100

    perf: show lightweight glb on homepage

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,30 @@
-const CACHE_NAME = 'model-cache-v1';
+const CACHE_NAME = "model-cache-v1";
 const ASSETS = [
-  'https://modelviewer.dev/shared-assets/models/Astronaut.glb',
-  'https://modelviewer.dev/shared-assets/environments/neutral.hdr'
+  "models/bag.glb",
+  "https://modelviewer.dev/shared-assets/environments/neutral.hdr",
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)),
   );
 });
 
-self.addEventListener('fetch', event => {
-  if (ASSETS.some(url => event.request.url.startsWith(url))) {
+self.addEventListener("fetch", (event) => {
+  if (ASSETS.some((url) => event.request.url.includes(url))) {
     event.respondWith(
-      caches.match(event.request).then(resp => {
-        return resp || fetch(event.request).then(response => {
-          const clone = response.clone();
-          caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
-          return response;
-        });
-      })
+      caches.match(event.request).then((resp) => {
+        return (
+          resp ||
+          fetch(event.request).then((response) => {
+            const clone = response.clone();
+            caches
+              .open(CACHE_NAME)
+              .then((cache) => cache.put(event.request, clone));
+            return response;
+          })
+        );
+      }),
     );
   }
 });
