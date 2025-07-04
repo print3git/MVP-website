5f9ebcbe227a43e71ff8d5101699239caea93682
print3git <JekyllC987123@outlook.com>
2025-07-04 18:10:41 +0100

    perf: accelerate model asset loading

--- service-worker.js
+++ service-worker.js
@@ -8,6 +8,11 @@ self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)),
   );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener("fetch", (event) => {
