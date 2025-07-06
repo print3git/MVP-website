7c448c68f329fd68fe9a43d1e2970762b5854411
print3git <JekyllC987123@outlook.com>
2025-07-06 12:13:57 +0100

    fix: purge outdated caches

--- service-worker.js
+++ service-worker.js
@@ -17,7 +17,16 @@ self.addEventListener("install", (event) => {
 });
 
 self.addEventListener("activate", (event) => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(
+          keys.map((key) => (key !== CACHE_NAME ? caches.delete(key) : null)),
+        ),
+      )
+      .then(() => self.clients.claim()),
+  );
 });
 
 self.addEventListener("fetch", (event) => {
