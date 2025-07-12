1bfa8889f9633e8a8e6269ef596d5c7407137a1e
print3git <JekyllC987123@outlook.com>
2025-07-12 22:52:00 +0100

    fix: preserve proxy variables for apt

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -10,7 +10,7 @@ cleanup_npm_cache() {
 trap cleanup_npm_cache EXIT
 cleanup_npm_cache
 
-unset npm_config_http_proxy npm_config_https_proxy http_proxy https_proxy
+unset npm_config_http_proxy npm_config_https_proxy
 export npm_config_fund=false
 
 # Provide a temporary Stripe key if none is configured so validation succeeds
