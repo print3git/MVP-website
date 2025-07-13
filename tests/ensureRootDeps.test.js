7626d05707484973780053e1362a2c0c969e6cf2
print3git <JekyllC987123@outlook.com>
2025-07-13 10:39:27 +0100

    fix: unset proxy vars during root deps install

--- tests/ensureRootDeps.test.js
+++ tests/ensureRootDeps.test.js
@@ -20,11 +20,27 @@ describe("ensure-root-deps", () => {
   test("retries on network failure", () => {
     fs.existsSync.mockReturnValue(false);
     child_process.execSync
+      .mockImplementationOnce(() => {})
       .mockImplementationOnce(() => {
         throw new Error("ECONNRESET");
       })
       .mockImplementation(() => {});
+    expect(() => require("../scripts/ensure-root-deps.js")).not.toThrow();
+  });
+
+  test("unsets npm proxy variables", () => {
+    fs.existsSync.mockReturnValue(false);
+    process.env.npm_config_http_proxy = "http://proxy";
+    process.env.npm_config_https_proxy = "http://proxy";
+    child_process.execSync.mockImplementation(() => {});
     require("../scripts/ensure-root-deps.js");
-    expect(child_process.execSync.mock.calls.length).toBeGreaterThanOrEqual(5);
+    const ciCall = child_process.execSync.mock.calls.find(
+      (c) => c[0] === "npm ci",
+    );
+    const env = ciCall ? ciCall[1].env : {};
+    expect(env.npm_config_http_proxy).toBeUndefined();
+    expect(env.npm_config_https_proxy).toBeUndefined();
+    delete process.env.npm_config_http_proxy;
+    delete process.env.npm_config_https_proxy;
   });
 });
