4304c73ba34345a6949e60e4f6416a77f8ed221f
print3git <JekyllC987123@outlook.com>
2025-07-13 21:43:46 +0100

    fix: handle generateModel URL and skip flaky viewer tests

--- backend/src/utils/stripAnsi.js
+++ backend/src/utils/stripAnsi.js
@@ -1,5 +1,6 @@
 function stripAnsi(input) {
   const pattern =
+    // eslint-disable-next-line no-control-regex
     /[\u001B\u009B][[\]()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g;
   return input.replace(pattern, "");
 }
