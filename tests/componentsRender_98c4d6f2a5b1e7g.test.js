5a946e078488b941287905536cf8c1933f263fb9
print3git <JekyllC987123@outlook.com>
2025-07-18 13:49:02 +0100

    test: add smoke test for CheckoutForm

---
+++ tests/componentsRender_98c4d6f2a5b1e7g.test.js
@@ -0,0 +1,38 @@
+/** @jest-environment jsdom */
+const React = require("react");
+const { render, screen } = require("@testing-library/react");
+require("@babel/register")({
+  presets: [[require.resolve("@babel/preset-react"), { runtime: "automatic" }]],
+  plugins: [
+    [require.resolve("@babel/plugin-syntax-typescript"), { isTSX: true }],
+  ],
+  extensions: [".js", ".jsx"],
+});
+
+jest.mock("react-places-autocomplete", () => {
+  const React = require("react");
+  return ({ value, onChange, children }) =>
+    React.createElement(
+      "div",
+      null,
+      children({
+        getInputProps: (props) => ({
+          ...props,
+          value,
+          onChange: (e) => onChange(e.target.value),
+        }),
+        suggestions: [],
+        getSuggestionItemProps: () => ({}),
+        loading: false,
+      }),
+    );
+});
+
+const CheckoutForm = require("../src/components/CheckoutForm.js").default;
+
+describe("component smoke tests", () => {
+  test("CheckoutForm renders", () => {
+    render(React.createElement(CheckoutForm));
+    expect(screen.getByRole("button", { name: /submit/i })).toBeTruthy();
+  });
+});
