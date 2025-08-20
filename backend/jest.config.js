ce3c5aaa5ee1237f3de6c429b0cb6ef7ba5c998d
print3git <JekyllC987123@outlook.com>
2025-08-20 19:24:52 +0100

    test: add shared test resource reaper

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -2,7 +2,10 @@
 module.exports = {
   rootDir: ".",
   setupFiles: ["<rootDir>/tests/setupGlobals.js"],
-  setupFilesAfterEnv: ["<rootDir>/tests/setup.js"],
+  setupFilesAfterEnv: [
+    "<rootDir>/../tests/utils/testEnv.ts",
+    "<rootDir>/tests/setup.js",
+  ],
   globalTeardown: "<rootDir>/tests/globalTeardown.js",
   testEnvironment: "node",
   transform: {
