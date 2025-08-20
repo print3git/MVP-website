51a1a9ba3c39778914ac01620cdcce9b952f341e
print3git <JekyllC987123@outlook.com>
2025-08-20 19:54:20 +0100

    test: retry flaky jest tests

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,7 @@ module.exports = {
   setupFilesAfterEnv: [
     "<rootDir>/test/setupAuthMiddleware.js",
     "<rootDir>/tests/setup/nock.ts",
+    "<rootDir>/tests/setup/retries.ts",
   ],
   coverageThreshold: {
     global: {
