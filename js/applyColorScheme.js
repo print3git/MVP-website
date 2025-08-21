b5493044b2ec369a20eddd6867934fa2edce077f
print3git <JekyllC987123@outlook.com>
2025-08-21 17:28:59 +0100

    fix: inline eslint ignores and guard browser globals

--- js/applyColorScheme.js
+++ js/applyColorScheme.js
@@ -1,7 +1,10 @@
-(function () {
-  try {
-    if (localStorage.getItem("colorScheme") === "light") {
-      document.documentElement.classList.add("light");
-    }
-  } catch {}
-})();
+/* eslint-env browser */
+if (typeof window !== "undefined") {
+  (function () {
+    try {
+      if (localStorage.getItem("colorScheme") === "light") {
+        document.documentElement.classList.add("light");
+      }
+    } catch {}
+  })();
+}
