4bebd935dd5d65f066af58fc3622e837633dba3e
print3git <JekyllC987123@outlook.com>
2025-09-02 20:32:19 +0100

    Create jest.setup.env.js

---
+++ backend/jest.setup.env.js
@@ -0,0 +1,3 @@
+process.env.FRONTEND_SUCCESS_URL = process.env.FRONTEND_SUCCESS_URL || "https://print2.io/index.html";
+process.env.FRONTEND_CANCEL_URL  = process.env.FRONTEND_CANCEL_URL  || "https://print2.io/payment.html";
+process.env.STRIPE_SECRET_KEY    = process.env.STRIPE_SECRET_KEY    || "sk_test_mock";
