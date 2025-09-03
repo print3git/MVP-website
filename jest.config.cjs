05c6f768031272ad73ecb150915d6cea4e914f62
print3git <JekyllC987123@outlook.com>
2025-09-04 01:32:28 +0800

    fix: enable babel transform for js tests

--- jest.config.cjs
+++ jest.config.cjs
@@ -9,6 +9,7 @@ try {
         "ts-jest",
         { tsconfig: "tsconfig.json", diagnostics: false },
       ],
+      "^.+\\.jsx?$": "babel-jest",
     },
     moduleFileExtensions: ["ts", "tsx", "js", "json"],
     passWithNoTests: true,
