ac658d7ec3d61553f63919f712034f7f820925d8
print3git <JekyllC987123@outlook.com>
2025-09-13 00:09:55 +0800

    test: stub navigation in index tests

--- tests/sdk/no-generateModel-export.spec.h7k3m2p9s4t8v6w1.ts
+++ tests/sdk/no-generateModel-export.spec.h7k3m2p9s4t8v6w1.ts
@@ -2,11 +2,23 @@ const entryPoints = ["../../js/api.js", "../../js/index.js"];
 
 describe("public SDK exports", () => {
   test("no generateModel symbol exported", () => {
-    for (const ep of entryPoints) {
-      const mod = require(ep);
-      for (const key of Object.keys(mod)) {
-        expect(/generateModel/i.test(key)).toBe(false);
+    const loc = window.location;
+    delete window.location;
+    window.location = {
+      ...loc,
+      assign: jest.fn(),
+      replace: jest.fn(),
+      href: "http://example.com",
+    };
+    try {
+      for (const ep of entryPoints) {
+        const mod = require(ep);
+        for (const key of Object.keys(mod)) {
+          expect(/generateModel/i.test(key)).toBe(false);
+        }
       }
+    } finally {
+      window.location = loc;
     }
   });
 });
